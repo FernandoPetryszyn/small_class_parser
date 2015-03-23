beb285e231a795a69851ad8d1f9b76a360a0312a
FernandoPetryszyn <fer_jjp@hotmail.com>
2015-03-23 13:55:55 -0300

    Parses a method with various sentences, parses a method with a long parameter list

--- src/test/scala/small_class_parser_test/RubyClassParserTest.scala
+++ src/test/scala/small_class_parser_test/RubyClassParserTest.scala
@@ -4,7 +4,7 @@ import small_class_parser.RubyClassParser
 
 class RubyClassParserTest extends ParserTest with RubyClassParser {
  "Simple class parser" - {
-   "should parse" - {
+   "should parse classes" - {
      "should parse an empty class" in {
        "class simpleClass {}" should beParsedTo(Program(List(Class("simpleClass",Nil))))(program)
      }
@@ -49,6 +49,19 @@ class RubyClassParserTest extends ParserTest with RubyClassParser {
        """class simpleClass { 
          def metodo1(){}
          def metodo2(){}}""" should beParsedTo(Program(List(Class("simpleClass",List(Method("metodo1",List(Empty),Nil),Method("metodo2",List(Empty),Nil))))))(program)
+     }
+   }
+   
+   "should parse methods" - {
+     "should parse a method with various definitions" in {
+       """def metodo() { 
+         1*2
+         2+1
+         a=3}""" should beParsedTo(Method("metodo",List(Empty),List(Multiply(Numero(1),Numero(2)),Add(Numero(2),Numero(1)),Define(Entero("a",Numero(3))))))(method)
+     }
+     
+     "should parse a method with a long parameter list" in {
+       """def metodo(a: int,b: int, c: int){}""" should beParsedTo(Method("metodo",List(Entero("a",Numero(0)),Entero("b",Numero(0)),Entero("c",Numero(0))),Nil))(method)
      }
    }
  }
