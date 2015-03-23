30434b45d1591a67c15ec8f70a99435bf9afb886
FernandoPetryszyn <fer_jjp@hotmail.com>
2015-03-23 12:29:40 -0300

    Parses a class with an initialized variable

--- src/test/scala/small_class_parser_test/RubyClassParserTest.scala
+++ src/test/scala/small_class_parser_test/RubyClassParserTest.scala
@@ -9,6 +9,11 @@ class RubyClassParserTest extends ParserTest with RubyClassParser {
        "class simpleClass {}" should beParsedTo(Program(List(Class("simpleClass",Nil))))(program)
      }
      
+     "should parse a class with  an instance variable initialized" in {
+       """class simpleClass {
+               a=3
+               }""" should beParsedTo(Program(List(Class("simpleClass",List(Entero("a",Numero(3)))))))(program)
+     }
    }
  }
 }
